635dc4238135a1c3d1b73f5a7c86008b1198b40e
Arkady Chesnokov <arkady-chesnokof@yandex.ru>
2025-07-22 18:07:23 +0300

    init cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use clap::{arg, Command};
+
+fn cli() -> Command {
+    Command::new("box")
+        .author("Arkady Chesnokov, arkady-chesnokof@yandex.ru")
+        .version("0.0.1")
+        .about("A fictional docker/youki clone")
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .override_usage("rust_box <SUBCOMMAND>")
+        .subcommand(
+            Command::new("build")
+                .about("Build image from boxfile")
+                .arg(
+                    arg!(-f --file <BOXFILE_PATH> "Path to boxfile")
+                )
+        )    
+}
+
 fn main() {
-    println!("Hello, world!");
+    let matches = cli().get_matches();
 }
